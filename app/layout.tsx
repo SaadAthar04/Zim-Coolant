de6ec801149a7f2d81b5da49491c7282ab7897a1
Saad Athar <saadathar55@gmail.com>
2025-11-25 22:56:05 +0500

    Add Google Analytics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import './globals.css'
 import { NavbarProvider } from '@/lib/navbar-context'
 
@@ -63,6 +64,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-C6HWTCS8SX"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-C6HWTCS8SX');
+          `}
+        </Script>
+      </head>
       <body>
         <NavbarProvider>
           {children}
